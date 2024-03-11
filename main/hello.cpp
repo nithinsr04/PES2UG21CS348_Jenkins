7550fb59aef53cb72a2b2bd1dc0d6cf26a1a8496
nithinsr04 <131294813+nithinsr04@users.noreply.github.com>
2024-03-11 23:31:58 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "My SRN is PES2UG21CS348\n";
+  cout << "I made a webhook which run on it's own on committing a task\n";
   return 0;
 }
